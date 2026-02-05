989cf1950a5081081ca353da942e71d111926bd1
WesleyKoerkamp <97099706@st.deltion.nl>
2026-02-05 15:37:03 +0100

    Opdrachten 040_lists Gemaakt

--- app/040_lists/opdr_1/opdr_1.py
+++ app/040_lists/opdr_1/opdr_1.py
@@ -1,9 +1,22 @@
 # Opdracht 1 lists
-# Naam student:
-# Groep:
+# Naam student: Wesley Koerkamp
+# Groep: 4ITX1
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+lijst = []
+
+dict1 = {"id": 0, "voornaam": "Jan", "achternaam": "Jansen"}
+dict2 = {"id": 1, "voornaam": "Piet", "achternaam": "Pieters"}
+dict3 = {"id": 2, "voornaam": "Sara", "achternaam": "Smit"}
+dict4 = {"id": 3, "voornaam": "Emma", "achternaam": "de Vries"}
+
+
+lijst.append(dict1)
+lijst.append(dict2)
+lijst.append(dict3)
+lijst.append(dict4)
+
+# Print de volledige naam van de 2e persoon
+print(lijst[0]["voornaam"], lijst[0]["achternaam"])
+print(lijst[1]["voornaam"], lijst[1]["achternaam"])
+print(lijst[2]["voornaam"], lijst[2]["achternaam"])
+print(lijst[3]["voornaam"], lijst[3]["achternaam"])
