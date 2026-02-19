5c84120e583ce3d277215bd7a78ca7f32d97009b
WesleyKoerkamp <97099706@st.deltion.nl>
2026-02-19 14:51:28 +0100

    Opdracht 3 van tekstbestanden helemaal aangepast en nu goed.

--- app/080_tekstbestanden/opdr_3/opdr_3.py
+++ app/080_tekstbestanden/opdr_3/opdr_3.py
@@ -2,21 +2,43 @@
 # Naam student: Wesley Koerkamp
 # Groep: 4ITX1
 
-tekst = input("Voer een tekst in die je wilt encrypten: \n")
-
-versleuteld = ""
-
-for letter in tekst:
-    if letter.isalpha():
-
-        if letter.isupper():
-            base = ord('A')
+def encrypt(tekst):
+    resultaat = ""
+    
+    for letter in tekst:
+        if letter.isalpha():  # Controleer of het een letter is
+            if letter.islower():
+                nieuwe_letter = chr((ord(letter) - ord('a') + 5) % 26 + ord('a'))
+            else:
+                nieuwe_letter = chr((ord(letter) - ord('A') + 5) % 26 + ord('A'))
+            resultaat += nieuwe_letter
         else:
-            base = ord('a')
+            resultaat += letter  # Spaties en leestekens blijven gelijk
+            
+    return resultaat
 
-    nieuwe_code = (ord(letter) - base + 1) % 26 + base
-    versleuteld += chr(nieuwe_code)
-else: 
-    versleuteld += letter
 
-print(versleuteld)
+def decrypt(tekst):
+    resultaat = ""
+    
+    for letter in tekst:
+        if letter.isalpha():
+            if letter.islower():
+                nieuwe_letter = chr((ord(letter) - ord('a') - 5) % 26 + ord('a'))
+            else:
+                nieuwe_letter = chr((ord(letter) - ord('A') - 5) % 26 + ord('A'))
+            resultaat += nieuwe_letter
+        else:
+            resultaat += letter
+            
+    return resultaat
+
+
+# Hoofdprogramma
+invoer = input("Geef de tekst die je wilt encrypten: ")
+
+versleuteld = encrypt(invoer)
+print("Versleuteld:", versleuteld)
+
+ontsleuteld = decrypt(versleuteld)
+print("Ontsleuteld:", ontsleuteld)
--- app/095_modules/opdr_1/opdr_1.py
+++ app/095_modules/opdr_1/opdr_1.py
@@ -3,3 +3,8 @@
 # Groep: 4ITX1
 
 # importeer de module csv...
+
+def lees_csv_bestand(bestandsnaam):
+    # je code komt hier
+    # het woordje pass hieronder kun je weghalen
+    pass
